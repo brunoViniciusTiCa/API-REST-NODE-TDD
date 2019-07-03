d56f1b0a7c93a8da19e6722f830f9c2198b6aad7
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-07-03 18:36:43 -0300

    Commit atualizado Db e test

--- src/app.js
+++ src/app.js
@@ -1,32 +1,23 @@
-/** Requisitanto os frames que seram usados, no caso
- *  expresse e o consign.
- */
 const express = require('express');
 const consign = require('consign');
 const knex = require('knex');
 const knexfile = require('../knexfile');
 
 const app = express();
-
 // TODO criar chaveamento dinamico
 app.db = knex(knexfile.test);
 
-/** Consign esta responsavel para trazer para o arquivo app as rotas e os middlewares,
- * e assim distribuindo a apliação e deixando mais partilhadas.
- */
 consign({
   cwd: 'src',
   verbose: false
 })
   .include('./config/middlewares.js')
-  .then('./config/routes.js')
   .then('./routes')
+  .then('./config/routes.js')
   .into(app);
 
-/** Rota raiz para teste. */
 app.get('/', (req, res) => {
   res.status(200).send();
 });
 
-/** Para fazer a exportação do APP */
 module.exports = app;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,17 +1,9 @@
-/** Esse module está exportando está aero function
- * para ser usado no arquivo routes.js com o findAll
- * e create.
- */
 module.exports = (app) => {
-  /** É uma função para achar tudo.
-   *  Como se fosse um select no usuario.
-   */
   const findAll = (req, res) => {
     app.db('users').select()
       .then(result => res.status(200).json(result));
   };
 
-  /** Para criar o usuario */
   const create = async (req, res) => {
     const result = await app.db('users').insert(req.body, '*');
     res.status(201).json(result[0]);
