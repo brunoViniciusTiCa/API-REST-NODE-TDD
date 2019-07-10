44ce2b94fb348cf48549c4b9f291fd7395647de9
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-07-10 17:02:00 -0300

    Refaturando a identação.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,9 @@
 module.exports = (app) => {
   const findAll = (req, res) => {
     app.services.user.findAll()
-      .then(result => res.status(200).json(result));
+      .then((result) => { 
+        res.status(200).json(result);
+    });
   };
 
   const create = async (req, res) => {
