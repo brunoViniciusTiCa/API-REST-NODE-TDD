90984b41937b37c7d3b66c32104627206b01deaa
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-07-13 11:06:08 -0300

    Refatorando

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = 3001;
 
 app.listen(process.env.PORT || port, function(){
-  console.log('Servidor subiu na porta: ' + port);
+  console.log('Servidor Rodando Na Porta: ' + port);
 });
