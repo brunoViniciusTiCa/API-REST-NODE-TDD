c0becfbc13d7e444a00a4525184322d7fd7ce3fa
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-07-19 09:59:42 -0300

    Documentando router e refatorando

--- src/config/router.js
+++ src/config/router.js
@@ -8,5 +8,13 @@ module.exports = (app) => {
   protectedRouter.use('/users',  app.routes.users);
   protectedRouter.use('/accounts',  app.routes.accounts);
 
+  /**v1 = versão 1 */
   app.use('/v1', app.config.passport.authenticate(), protectedRouter);
+
+  /** Segunda rota não esta protegida e foi apenas para testar que desta forma de criar nossas
+   * rotas, temos mais autonomia sobre nosso codigo e também podemos herdar e assim o codigo 
+   * ficará menor e legivel.
+   */
+  app.get('/v2/users', (req, res) => res.status(200).send('V2 esta rodando na segunda rota sem está protegida.'));
+  app.use('/v2', protectedRouter);
 };
