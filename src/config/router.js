10449792bc2424cd63b3b408fcfb331118696492
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-07-19 09:28:50 -0300

    comit refatoramento

--- src/config/router.js
+++ src/config/router.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 module.exports = (app) => {
   app.use('/auth', app.routes.auth);
-  
   app.use('/users', app.config.passport.authenticate(), app.routes.users);
-  
   app.use('/accounts', app.config.passport.authenticate(), app.routes.accounts);
-
-  //app.use('/accounts', app.routes.accounts);
 };
