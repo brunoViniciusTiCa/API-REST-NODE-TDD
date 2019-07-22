d4b5dbcb1f4082110019950cdda1d03f42864944
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-07-22 19:17:32 -0300

    Criando router, services e refatorando.

--- test/routes/transactions.test.js
+++ test/routes/transactions.test.js
@@ -20,13 +20,13 @@ beforeAll(async () => {
    delete user.passwd;
    user.token = jwt.encode(user, 'Segredo');
    const accs = await app.db('accounts').insert([
-     { name: 'acc #1', users_id = user.id },
-     { name: 'acc #2', users_id = user2.id }
+     { name: 'Acc #1', users_id = user.id },
+     { name: 'Acc #2', users_id = user2.id }
    ], '*');
    [accUser, accUser2] = accs;
 });
 
-test('Deve listar apenas a transações do usuario' , () => {
+test('Deve listar apenas a transações do usuario', () => {
   return app.db('transactions').insert([
     { description: 'T1', date: new Date(), ammount: 100,  type: 'I', acc_id: accUser.id },
     { description: 'T2', date: new Date(), ammount: 300,  type: 'O', acc_id: accUser.id }
@@ -35,6 +35,6 @@ test('Deve listar apenas a transações do usuario' , () => {
   .then((res) => {
     expect(res.status).toBe(200);
     expect(res.body).toHaveLength(1);
-    expect(res.body.description).toBe('T1');
+    expect(res.body[0].description).toBe('T1');
   }));
 });
