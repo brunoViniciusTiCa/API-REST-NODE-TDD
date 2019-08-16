c309244f820ebbcdce728cbfdeec9cfc3f5e0ebe
Bruno Vinicius Felix Da Silva <brunoviniciustica@gmail.com>
2019-08-16 13:08:11 -0300

    Commit transferencia test

--- src/config/router.js
+++ src/config/router.js
@@ -8,20 +8,12 @@ module.exports = (app) => {
   protectedRouter.use('/users',  app.routes.users);
   protectedRouter.use('/accounts',  app.routes.accounts);
   protectedRouter.use('/transactions', app.routes.transactions);
+  protectedRouter.use('/transfers', app.routes.transfers);
 
-  /**v1 = versão 1 Na rota V1 todas as middlewares estão protegidas com o JWT, Token e autenticadas. 
-   * Desta nova forma criei um arquivo, de router e nele coloquei uma lógica onde pode ser feito o caminho
-   * alterando e assim deixando cada rota tendo autonomia. Validando as rotas V1 e V1/users, avisando que precisa
-   * fazer o login na conta. 
-   */
   app.get('/v1', (req, res) => res.status(200).send('Faça o login. Você não esta autorizado!'));
   app.get('/v1/users', (req, res) => res.status(200).send('Você não esta autorizado!'));
   app.use('/v1', app.config.passport.authenticate(), protectedRouter);
 
-  /** Segunda rota não esta protegida e foi apenas para testar que desta forma de criar nossas
-   * rotas, temos mais autonomia sobre nosso codigo e também podemos herdar e assim o codigo 
-   * ficará menor e legivel.
-   */
   app.get('/v2/users', (req, res) => res.status(200).send('V2 esta rodando na segunda rota sem está protegida.'));
   app.use('/v2', protectedRouter);
 };
---
+++ test/routes/transfers.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+const MAIN_ROUTE = '/v1/tranfers';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwMDAsIm5hbWUiOiJMb25ndWluaG8gdXNlciAjMSIsIm1haWwiOiJ1c2VyMUBnbWFpbC5jb20ifQ.ONlO-k5tuViXjrTlpDonO2g8YO7cMzKZsrlpi43Z4v8'
+
+test('Deve listar apenas transferências do usuário', () => {
+  return request(app).get(MAIN_ROUTE)
+    .set('authorization', `bearer ${TOKEN}`)
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveLength(1);
+      expect(res.body[0].description).toBe('Transfer user#1');
+    });
+});
